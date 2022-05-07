6e74fc4990a746218b565004bb1a4c1932ce3ea5
Joachimchisom1 <joachimchisom1@gmail.com>
2022-05-07 20:58:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import EmployeeContextProvider from './contexts/EmployeeContext'
+import EmployeeList from './components/EmployeeList'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-xl">
+      <div className="table-responsive">
+        <div className="table-wrapper">
+          <EmployeeContextProvider>
+          <EmployeeList />
+          </EmployeeContextProvider>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
